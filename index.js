bab555c4c0fd97414c6587c2776c86fa9491ef1c
riqi3dev <riki.iiidev@gmail.com>
2024-09-17 18:28:21 +0800

    finalized dice logic and design

--- index.js
+++ index.js
@@ -1,172 +1,191 @@
-
 let title = document.getElementById("title");
-let itemList = document.getElementById("grid-container");
-let diceDotGridContainer = itemList.querySelector("div.item #dice-dot-grid-container");  
-let diceDot1 = diceDotGridContainer.querySelector("#dot-1");
-let diceDot2 = diceDotGridContainer.querySelector("#dot-2");
-let diceDot3 = diceDotGridContainer.querySelector("#dot-3");
-let diceDot4 = diceDotGridContainer.querySelector("#dot-4");
-let diceDot5 = diceDotGridContainer.querySelector("#dot-5");
-let diceDot6 = diceDotGridContainer.querySelector("#dot-6");
+
+let itemList = document.getElementById("grid-container");  
+let diceDotGridContainerP1 = itemList.children[0];
+let diceDotGridContainerP2 = itemList.children[1];
+
+let diceDot1P1 = diceDotGridContainerP1.querySelector("#dot-1-P1");
+let diceDot2P1 = diceDotGridContainerP1.querySelector("#dot-2-P1");
+let diceDot3P1 = diceDotGridContainerP1.querySelector("#dot-3-P1");
+let diceDot4P1 = diceDotGridContainerP1.querySelector("#dot-4-P1");
+let diceDot5P1 = diceDotGridContainerP1.querySelector("#dot-5-P1");
+let diceDot6P1 = diceDotGridContainerP1.querySelector("#dot-6-P1");
+
+let diceDot1P2 = diceDotGridContainerP2.querySelector("#dot-1-P2");
+let diceDot2P2 = diceDotGridContainerP2.querySelector("#dot-2-P2");
+let diceDot3P2 = diceDotGridContainerP2.querySelector("#dot-3-P2");
+let diceDot4P2 = diceDotGridContainerP2.querySelector("#dot-4-P2");
+let diceDot5P2 = diceDotGridContainerP2.querySelector("#dot-5-P2");
+let diceDot6P2 = diceDotGridContainerP2.querySelector("#dot-6-P2");
 
 function diceOutput() {
   let diceRollP1 = Math.floor(Math.random() * 6) + 1; 
   let diceRollP2 = Math.floor(Math.random() * 6) + 1; 
 
-  playerOne(diceRollP1);
-  playerTwo(diceRollP2);
   if (diceRollP1 > diceRollP2) {
     title.innerHTML = "🚩 Player 1 Wins!";
   } else if (diceRollP2 > diceRollP1) {
     title.innerHTML = "Player 2 Wins! 🚩";
   } else {
     title.innerHTML = "🚩 Draw! 🚩";
-  }
- 
-}
+  } 
 
-function playerOne(diceRollP1) {
   switch (diceRollP1) {
     case 1:
-      diceRoll1();
+      diceRoll1P1();
       break;
     case 2:
-      diceRoll2();
+      diceRoll2P1();
       break;
     case 3:
-      diceRoll3();
+      diceRoll3P1();
       break;
     case 4:
-      diceRoll4();
+      diceRoll4P1();
       break;
     case 5:
-      diceRoll5();
+      diceRoll5P1();
       break;
     case 6:
-      diceRoll6();
+      diceRoll6P1();
       break;
     default:
       break;
-  }
-}
-
-function playerTwo(diceRollP2) {
+  } 
+  
   switch (diceRollP2) {
     case 1:
-      diceRoll1();
+      diceRoll1P2();
       break;
     case 2:
-      diceRoll2();
+      diceRoll2P2();
       break;
     case 3:
-      diceRoll3();
+      diceRoll3P2();
       break;
     case 4:
-      diceRoll4();
+      diceRoll4P2();
       break;
     case 5:
-      diceRoll5();
+      diceRoll5P2();
       break;
     case 6:
-      diceRoll6();
+      diceRoll6P2();
       break;
     default:
       break;
-  }
+  } 
+}
+ 
+
+function diceRoll6P1() { 
+  diceDot1P1.style.gridArea = "one";
+  diceDot2P1.style.gridArea = "five";
+  diceDot3P1.style.gridArea = "six";
+  diceDot4P1.style.gridArea = "seven";
+  diceDot5P1.style.gridArea = "eight";
+  diceDot6P1.style.gridArea = "three"; 
 }
 
-function diceRoll6() {
-  gridContainerStyle(); 
-  diceDot1.style.gridArea = "one";
-  diceDot2.style.gridArea = "five";
-  diceDot3.style.gridArea = "six";
-  diceDot4.style.gridArea = "seven";
-  diceDot5.style.gridArea = "eight";
-  diceDot6.style.gridArea = "three";
-  addDot("div.dot#dot-1");
-  addDot("div.dot#dot-2");
-  addDot("div.dot#dot-3");
-  addDot("div.dot#dot-4");
-  addDot("div.dot#dot-5");
-  addDot("div.dot#dot-6");
+function diceRoll5P1() {
+  diceDot1P1.style.gridArea = "one";
+  diceDot2P1.style.gridArea = "two";
+  diceDot3P1.style.gridArea = "three";
+  diceDot4P1.style.gridArea = "eight";
+  diceDot5P1.style.gridArea = "five";
+  diceDot6P1.style.gridArea = "five"; 
 }
 
-function diceRoll5() {
-  gridContainerStyle();
-  diceDot1.style.gridArea = "one";
-  diceDot2.style.gridArea = "two";
-  diceDot3.style.gridArea = "three";
-  diceDot4.style.gridArea = "eight";
-  diceDot5.style.gridArea = "five";
-  addDot("div.dot#dot-1");
-  addDot("div.dot#dot-2");
-  addDot("div.dot#dot-3");
-  addDot("div.dot#dot-4");
-  addDot("div.dot#dot-5");
-  removeDot("div.dot#dot-6");
+function diceRoll4P1() {
+  diceDot1P1.style.gridArea = "one";
+  diceDot2P1.style.gridArea = "five";
+  diceDot3P1.style.gridArea = "three";
+  diceDot4P1.style.gridArea = "eight";
+  diceDot1P1.style.gridArea = "one";
+  diceDot2P1.style.gridArea = "five";  
 }
 
-function diceRoll4() {
-  gridContainerStyle();
-  diceDot1.style.gridArea = "one";
-  diceDot2.style.gridArea = "five";
-  diceDot3.style.gridArea = "three";
-  diceDot4.style.gridArea = "eight";
-  addDot("div.dot#dot-1");
-  addDot("div.dot#dot-2");
-  addDot("div.dot#dot-3");
-  addDot("div.dot#dot-4");
-  removeDot("div.dot#dot-5");
-  removeDot("div.dot#dot-6");
+function diceRoll3P1() {
+  diceDot1P1.style.gridArea = "one";
+  diceDot2P1.style.gridArea = "two";
+  diceDot3P1.style.gridArea = "three";
+  diceDot4P1.style.gridArea = "one";
+  diceDot5P1.style.gridArea = "two";
+  diceDot6P1.style.gridArea = "three"; 
 }
 
-function diceRoll3() {
-  gridContainerStyle();
-  diceDot1.style.gridArea = "one";
-  diceDot2.style.gridArea = "two";
-  diceDot3.style.gridArea = "three";
-  addDot("div.dot#dot-1");
-  addDot("div.dot#dot-2");
-  addDot("div.dot#dot-3");
-  removeDot("div.dot#dot-4");
-  removeDot("div.dot#dot-5");
-  removeDot("div.dot#dot-6");
+function diceRoll2P1() {
+  diceDot1P1.style.gridArea = "one";
+  diceDot2P1.style.gridArea = "three";
+  diceDot3P1.style.gridArea = "one";
+  diceDot4P1.style.gridArea = "three";
+  diceDot5P1.style.gridArea = "one";
+  diceDot6P1.style.gridArea = "three"; 
 }
 
-function diceRoll2() {
-  gridContainerStyle();
-  diceDot1.style.gridArea = "one";
-  diceDot2.style.gridArea = "three";
-  addDot("div.dot#dot-1");
-  addDot("div.dot#dot-2");
-  removeDot("div.dot#dot-3");
-  removeDot("div.dot#dot-4");
-  removeDot("div.dot#dot-5");
-  removeDot("div.dot#dot-6");
+function diceRoll1P1() { 
+  diceDot1P1.style.gridArea = "two";
+  diceDot2P1.style.gridArea = "two";
+  diceDot3P1.style.gridArea = "two";
+  diceDot4P1.style.gridArea = "two";
+  diceDot5P1.style.gridArea = "two";
+  diceDot6P1.style.gridArea = "two"; 
+}
+ 
+
+
+
+function diceRoll6P2() { 
+  diceDot1P2.style.gridArea = "one";
+  diceDot2P2.style.gridArea = "five";
+  diceDot3P2.style.gridArea = "six";
+  diceDot4P2.style.gridArea = "seven";
+  diceDot5P2.style.gridArea = "eight";
+  diceDot6P2.style.gridArea = "three"; 
 }
 
-function diceRoll1() { 
-  gridContainerStyle();
-  diceDot1.style.gridArea = "two";
-  addDot("div.dot#dot-1");
-  removeDot("div.dot#dot-2");
-  removeDot("div.dot#dot-3");
-  removeDot("div.dot#dot-4");
-  removeDot("div.dot#dot-5");
-  removeDot("div.dot#dot-6");
+function diceRoll5P2() {
+  diceDot1P2.style.gridArea = "one";
+  diceDot2P2.style.gridArea = "two";
+  diceDot3P2.style.gridArea = "three";
+  diceDot4P2.style.gridArea = "eight";
+  diceDot5P2.style.gridArea = "five";
+  diceDot6P2.style.gridArea = "five"; 
 }
 
-function removeDot(name) {
-  document.querySelector(name).classList.add("invisible");
-}
-function addDot(name) {
-  document.querySelector(name).classList.remove("invisible");
+function diceRoll4P2() {
+  diceDot1P2.style.gridArea = "one";
+  diceDot2P2.style.gridArea = "five";
+  diceDot3P2.style.gridArea = "three";
+  diceDot4P2.style.gridArea = "eight";
+  diceDot1P2.style.gridArea = "one";
+  diceDot2P2.style.gridArea = "five";  
 }
 
-function gridContainerStyle() {
-  diceDotGridContainer.style.position = "relative";
-  diceDotGridContainer.style.display = "grid";
-  diceDotGridContainer.style.gridTemplateAreas = `"one four five" "six two seven" "eight nine three"`;
-  diceDotGridContainer.style.columnGap = "0.5rem";
-  diceDotGridContainer.style.rowGap = "1rem";
+function diceRoll3P2() {
+  diceDot1P2.style.gridArea = "one";
+  diceDot2P2.style.gridArea = "two";
+  diceDot3P2.style.gridArea = "three";
+  diceDot4P2.style.gridArea = "one";
+  diceDot5P2.style.gridArea = "two";
+  diceDot6P2.style.gridArea = "three"; 
 }
+
+function diceRoll2P2() {
+  diceDot1P2.style.gridArea = "one";
+  diceDot2P2.style.gridArea = "three";
+  diceDot3P2.style.gridArea = "one";
+  diceDot4P2.style.gridArea = "three";
+  diceDot5P2.style.gridArea = "one";
+  diceDot6P2.style.gridArea = "three"; 
+}
+
+function diceRoll1P2() { 
+  diceDot1P2.style.gridArea = "two";
+  diceDot2P2.style.gridArea = "two";
+  diceDot3P2.style.gridArea = "two";
+  diceDot4P2.style.gridArea = "two";
+  diceDot5P2.style.gridArea = "two";
+  diceDot6P2.style.gridArea = "two"; 
+}
+ 
