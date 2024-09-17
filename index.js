92b933475324b5b24685452eae1987886910d0f3
riqi3dev <riki.iiidev@gmail.com>
2024-09-17 17:40:43 +0800

    updated logic

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-let player1 = document.getElementsByClassName("item")[0];
-let diceDotGridContainer = player1.querySelector(
-  ".dice > #dice-dot-grid-container"
-); 
- 
+
 let title = document.getElementById("title");
-let diceDot1 = document.getElementById("dot-1");
-let diceDot2 = document.getElementById("dot-2");
-let diceDot3 = document.getElementById("dot-3");
-let diceDot4 = document.getElementById("dot-4");
-let diceDot5 = document.getElementById("dot-5");
-let diceDot6 = document.getElementById("dot-6");
+let itemList = document.getElementById("grid-container");
+let diceDotGridContainer = itemList.querySelector("div.item #dice-dot-grid-container");  
+let diceDot1 = diceDotGridContainer.querySelector("#dot-1");
+let diceDot2 = diceDotGridContainer.querySelector("#dot-2");
+let diceDot3 = diceDotGridContainer.querySelector("#dot-3");
+let diceDot4 = diceDotGridContainer.querySelector("#dot-4");
+let diceDot5 = diceDotGridContainer.querySelector("#dot-5");
+let diceDot6 = diceDotGridContainer.querySelector("#dot-6");
 
 function diceOutput() {
-  let diceRollP1 = Math.floor(Math.random() * 6) + 1;
-  let diceRollP2 = Math.floor(Math.random() * 6) + 1;
+  let diceRollP1 = Math.floor(Math.random() * 6) + 1; 
+  let diceRollP2 = Math.floor(Math.random() * 6) + 1; 
+
+  playerOne(diceRollP1);
+  playerTwo(diceRollP2);
   if (diceRollP1 > diceRollP2) {
     title.innerHTML = "🚩 Player 1 Wins!";
-  } else {
+  } else if (diceRollP2 > diceRollP1) {
     title.innerHTML = "Player 2 Wins! 🚩";
+  } else {
+    title.innerHTML = "🚩 Draw! 🚩";
   }
-  playerOne(diceRollP1);
+ 
 }
 
 function playerOne(diceRollP1) {
@@ -47,13 +50,33 @@ function playerOne(diceRollP1) {
   }
 }
 
+function playerTwo(diceRollP2) {
+  switch (diceRollP2) {
+    case 1:
+      diceRoll1();
+      break;
+    case 2:
+      diceRoll2();
+      break;
+    case 3:
+      diceRoll3();
+      break;
+    case 4:
+      diceRoll4();
+      break;
+    case 5:
+      diceRoll5();
+      break;
+    case 6:
+      diceRoll6();
+      break;
+    default:
+      break;
+  }
+}
+
 function diceRoll6() {
-  gridContainerStyle();
-  diceDotGridContainer.style.display = "grid";
-  diceDotGridContainer.style.gridTemplateColumns = "auto auto";
-  diceDotGridContainer.style.gridTemplateRows = "auto auto auto";
-  diceDotGridContainer.style.columnGap = "0.5rem";
-  diceDotGridContainer.style.rowGap = "1rem";
+  gridContainerStyle(); 
   diceDot1.style.gridArea = "one";
   diceDot2.style.gridArea = "five";
   diceDot3.style.gridArea = "six";
@@ -122,10 +145,9 @@ function diceRoll2() {
   removeDot("div.dot#dot-6");
 }
 
-function diceRoll1() {
-  diceDotGridContainer.style.display = "flex";
-  diceDotGridContainer.style.alignItems = "center";
-  diceDotGridContainer.style.justifyItems = "center";
+function diceRoll1() { 
+  gridContainerStyle();
+  diceDot1.style.gridArea = "two";
   addDot("div.dot#dot-1");
   removeDot("div.dot#dot-2");
   removeDot("div.dot#dot-3");
